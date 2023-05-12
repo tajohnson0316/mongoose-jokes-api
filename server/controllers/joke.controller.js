5601675f38cf26dec7bf76a15f5397977873a315
tajohnson0316 <armanijohnson1331@gmail.com>
2023-05-12 15:04:05 -0500

    Completed all CRUD-necessary routes

--- server/controllers/joke.controller.js
+++ server/controllers/joke.controller.js
@@ -2,6 +2,10 @@ const Joke = require("../models/joke.model");
 
 // *** GET ALL ***
 module.exports.findAllJokes = (request, response) => {
+  /**
+   * .find()
+     @param: none 
+   */
   Joke.find()
     .then((allJokes) => {
       // Response console.log:
@@ -14,14 +18,77 @@ module.exports.findAllJokes = (request, response) => {
     });
 };
 
+// *** GET ONE ***
+module.exports.findOneJoke = (request, response) => {
+  /** 
+    .findOne({_id: request.params.id})
+    @param: the passed in "id" from the URL
+  */
+  Joke.findOne({ _id: request.params.id })
+    .then((oneJoke) => {
+      // Response console.log:
+      console.log({ results: oneJoke });
+
+      response.json({ results: oneJoke });
+    })
+    .catch((error) => {
+      response.json({ message: "Something went wrong...", error });
+    });
+};
+
 // *** CREATE ONE ***
 module.exports.createNewJoke = (request, response) => {
+  /** 
+    .create({request.body})
+    @param: the body/data of the client request
+  */
   Joke.create(request.body)
     .then((newJoke) => {
       // Response console.log:
       console.log({ results: newJoke });
 
       response.json({ results: newJoke });
+    })
+    .catch((error) => {
+      response.json({ message: "Something went wrong...", error });
+    });
+};
+
+// *** UPDATE ONE ***
+module.exports.updateExistingJoke = (request, response) => {
+  /**
+    .findOneAndUpdate({_id: request.params.id}, request.body, {new, runValidators})
+    @param: the passed in "id" from the URL
+    @param: the body/data of the client request
+    @param: update options
+  */
+  Joke.findOneAndUpdate({ _id: request.params.id }, request.body, {
+    new: true,
+    runValidators: true,
+  })
+    .then((updatedJoke) => {
+      // Response console.log:
+      console.log({ results: updatedJoke });
+
+      response.json({ results: updatedJoke });
+    })
+    .catch((error) => {
+      response.json({ message: "Something went wrong...", error });
+    });
+};
+
+// *** UPDATE ONE ***
+module.exports.deleteExistingJoke = (request, response) => {
+  /**
+    .findOneAndUpdate({_id: request.params.id})
+    @param: the passed in "id" from the URL
+  */
+  Joke.deleteOne({ _id: request.params.id })
+    .then((deletedJoke) => {
+      // Response console.log:
+      console.log({ results: deletedJoke });
+
+      response.json({ results: deletedJoke });
     })
     .catch((error) => {
       response.json({ message: "Something went wrong...", error });
--- server/routes/joke.routes.js
+++ server/routes/joke.routes.js
@@ -4,6 +4,15 @@ module.exports = (app) => {
   // GET ALL route
   app.get("/api/jokes", JokeController.findAllJokes);
 
+  // GET ONE route
+  app.get("/api/jokes/:id", JokeController.findOneJoke);
+
   // CREATE ONE route
   app.post("/api/jokes", JokeController.createNewJoke);
+
+  // UPDATE ONE route
+  app.patch("/api/jokes/:id", JokeController.updateExistingJoke);
+
+  // DELETE ONE route
+  app.delete("/api/jokes/:id", JokeController.deleteExistingJoke);
 };
